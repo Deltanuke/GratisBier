0e8248f00d84aafde13b0ef3bdb4c7b66d41462e
Pelle de Greeuw <pelledegreeuw97@gmail.com>
2017-04-11 08:57:57 +0200

    final 0.9alpha(rev b)

--- Colorize.py
+++ Colorize.py
@@ -25,6 +25,10 @@ create a new 'new list' and start the process over again
 """
 
 
+mode = 1   2  # 0 = count automorphisms between graphs, 1 = count automorphisms in a singel graph, 2 = detect isomorphism
+input_file = "input/basic/basicGI1.grl"
+
+
 def colorize_graph(gr: Graph):
     d = dict()
     index = 0
@@ -52,7 +56,7 @@ def colorize_graph(gr: Graph):
     colorize(q, index)
 
 
-def colorize_list(graphs : list):
+def colorize_list(graphs : list, auto: bool = False, single: bool = False):
     d = dict()
     index = 0
 
@@ -70,24 +74,86 @@ def colorize_list(graphs : list):
     # prepare for loop
     q = list()
     # change dict to list to be iterated updon
+    index = 0
     for vertices in d.values():
+        for vertex in vertices:
+            vertex.color = index
+            vertex.color_next = index
         q.append(vertices)
+        index += 1
 
     # set the index correctly
     index += 1
 
     q, index = colorize(q, index)
     main_iso = is_iso(q, graphs)
-    filtered_isos = filter_bijections(main_iso, q)
+    filtered_isos = main_iso
     if len(filtered_isos) > 0:
         tuples = create_tuples_from_isos(filtered_isos)
-        index += 1
+        if single:
+            tuples = list()
+            for i in range(int(len(graphs) / 2)):
+                tuples.append([graphs[i], graphs[(i + int(len(graphs)/2))]])
+        for list_vertices in q:
+            for vertex in list_vertices:
+                if vertex.color >= index:
+                    index = vertex.color + 1
+        triples = list()
         for graph1, graph2 in tuples:
-            print("Testing graph " + str(graph1.id) + " against graph " + str(graph2.id))
+            # print("Testing graph " + str(graph1.id) + " against graph " + str(graph2.id))
             plist = purify_list(q, graph1, graph2)
-            count = count_ismorphisms(plist, graph1, graph2, index, True)
-            print("Graph " + str(graph1.id) + " is ismorphic with graph " + str(graph2.id) + ", with " + str(count) +
-                  " isomorphishms")
+            count = count_isomorphisms(plist, graph1, graph2, index, list(), list(), auto)
+            if count > 0:
+                if graph1.id <= graph2.id:
+                    triples.append([graph1, graph2, count])
+                else:
+                    triples.append([graph2, graph1, count])
+
+        if auto and single:
+            print("Graph: Number of automorphisms:")
+            for graph1, graph2, count in triples:
+                print("{}:    {}".format(graph1.id, count))
+        elif auto:
+            print("Sets of isomorphic graphs: Number of automorphisms:")
+            for graph1, graph2, count in triples:
+                print("[{},{}]                {}".format(graph1.id, graph2.id, count))
+        else:
+            print("Sets of isomorphic graphs:")
+            isomorphs = list()
+            for graph1, graph2, count in triples:
+                if len(isomorphs) == 0:
+                    isomorphs.append([graph1, graph2])
+                else:
+                    for l in isomorphs:
+                        if graph1 in l and graph2 not in l:
+                            l.append(graph2)
+                            break
+                        elif graph2 in l and graph1 not in l:
+                            l.append(graph1)
+                            break
+                        elif graph2 not in l and graph1 not in l:
+                            isomorphs.append([graph1, graph2])
+                            break
+            for l in isomorphs:
+                sys.stdout.write("[")
+                for graph in l:
+                    sys.stdout.write("{}, ".format(graph.id))
+                print("]")
+
+
+def sort_triples(triples):
+    ret = list()
+    for graph1, graph2, count in triples:
+        id = 0
+        if len(ret) == 0:
+            ret.append([graph1, graph2, count])
+            continue
+        for graph11, graph22, countt in ret:
+            if graph11.id > graph1.id:
+                id = ret.index([graph11, graph22, countt])
+                break
+        ret.insert(id, [graph1, graph2, count])
+
 
 def create_tuples_from_isos(dict_isos: dict) -> list:
     tuples = list()
@@ -142,52 +208,108 @@ def purify_list(main_list: list, graph1: Graph, graph2: Graph) -> list:
     return return_list
 
 
-def count_ismorphisms(list_vertices: list, graph1: Graph, graph2: Graph, index: int, parent: bool = False) -> int:
+def count_isomorphisms(list_vertices: list, graph1: Graph, graph2: Graph, index: int,
+                       servicable_vertices: list = list(), automorphisms: list = list(), auto: bool = False):
     if is_unbalanced(list_vertices):
         return 0
     if is_bijection(list_vertices, graph1, graph2):
-        return 1
+        res = dict()
+        for vertices in servicable_vertices:
+            for v2 in vertices:
+                if v2.graph == graph2:
+                    res[v2.label] = v2.color_next
+        # print("found bijection: {}".format(check_dict_in_list(automorphisms, res)))
+        if not check_dict_in_list(automorphisms, res):
+            automorphisms.append(res)
+            return 1
+        return 0
     num = 0
-    index += 1
     color_classes = get_color_classes(list_vertices)
-    for color_class in color_classes:
-        vertex = get_first_vertex(color_class, graph1)
-        vertex.color = index
-        vertex.color_next = index
-        color_class.remove(vertex)
-        if parent:
-            print(vertex.label)
-        for change_vertex in color_class:
-            if change_vertex.graph == graph2:
-                change_vertex.color = index
-                change_vertex.color_next = index
-                color_class.remove(change_vertex)
-                llist = list()
-                llist.append(vertex)
-                llist.append(change_vertex)
-                list_vertices.append(llist)
-                q, indext = colorize_faster(list_vertices.copy(), index)
-                list_vertices.remove(llist)
-                color_class.append(change_vertex)
-                with open('output/out_index{}_{}.dot'.format(vertex.label, index), 'w') as _file:
-                    write_dot(vertex.graph, _file)
-                num += count_ismorphisms(q.copy(), graph1, graph2, index)
-                col = 0
-                for vertices in list_vertices:
-                    for vertex_col in vertices:
-                        vertex_col.color = col
-                        vertex_col.color_next = col
-                    col += 1
-                vertex.color = index
-                vertex._color_next = index
-                if num >= 1:
-                    vertex.color = index - 1
-                    vertex.color_next = index - 1
-                    return num
-        vertex.color = index - 1
-        vertex.color_next = index - 1
-        color_class.append(vertex)
+    if len(servicable_vertices) == 0:
+        servicable_vertices = color_classes.copy()
+    color_class = color_classes[0]
+    vertex = get_first_vertex(color_class, graph1)
+    vertex.color = index
+    vertex.color_next = index
+    # print("vertex {}: {}".format(vertex.graph.id, vertex.label))
+    color_class.remove(vertex)
+    inc_col = dict()
+    for vertices in list_vertices:
+        for v2 in vertices:
+            inc_col[v2] = v2.color
+    for change_vertex in color_class.copy():
+        if change_vertex.graph == graph2:
+            # print("vertex {}: {}".format(change_vertex.graph.id, change_vertex.label))
+            change_vertex.color = index
+            change_vertex.color_next = index
+            color_class.remove(change_vertex)
+            llist = list()
+            llist.append(vertex)
+            llist.append(change_vertex)
+            list_vertices.append(llist)
+            q, indext = colorize(list_vertices.copy(), index + 1)
+            list_vertices.remove(llist)
+            color_class.append(change_vertex)
+            # print("down vvvvvvvvvvvvvvv")
+            num += count_isomorphisms(q.copy(), graph1, graph2, indext + 1, servicable_vertices, automorphisms, auto)
+            # print("up ^^^^^^^^^^^^^")
+            if not auto and num > 0:
+                return num
+            for v2 in inc_col.keys():
+                v2.color = inc_col[v2]
+                v2.color_next = inc_col[v2]
+            if not auto and num > 0:
+                vertex.color = color_class[0].color
+                vertex.color_next = color_class[0].color
+                color_class.append(vertex)
+                return num
+    vertex.color = color_class[0].color
+    vertex.color_next = color_class[0].color
+    color_class.append(vertex)
+    # print(len(automorphisms))
     return num
+
+
+def check_dict_in_list(l: list, d: dict):
+    if len(l) == 0:
+        return False
+    for dt in l:
+        found = True
+        for key in d.keys():
+            if key not in dt.keys():
+                found = False
+                break
+            if dt[key] != d[key]:
+                found = False
+                break
+        if found:
+            return True
+    return False
+
+
+def merge_lists(l1: list, l2: list) -> list():
+    res = list()
+    l1_deeper = None
+    l2_deeper = None
+    for l1_item in l1:
+        if type(l1_item) is int:
+            if l1_item not in res:
+                res.append(l1_item)
+        else:
+            l1_deeper = l1_item
+    for l2_item in l2:
+        if type(l2_item) is int:
+            if l2_item not in res:
+                res.append(l2_item)
+        else :
+            l2_deeper = l2_item
+    if l2_deeper and l1_deeper:
+        res.append(merge_lists(l2_deeper, l1_deeper))
+    elif l1_deeper:
+        res.append(l1_deeper)
+    else:
+        res.append(l2_deeper)
+    return res
 
 
 def get_color_class(main_list: list) -> list:
@@ -251,13 +373,13 @@ def is_iso(list_of_list_verticecs : list, graph_list: list) -> dict:
                     except ValueError:
                         pass
     remove_duplicate_isos(main_graphs)
-    for graph in main_graphs.keys():
-        sys.stdout.write("Graph: " + str(graph.id) + " is iso with: ")
-        sys.stdout.flush()
-        for gr in main_graphs[graph]:
-            sys.stdout.write(str(gr.id) + ", ")
-        sys.stdout.write("\n")
-        sys.stdout.flush()
+    # for graph in main_graphs.keys():
+    #     sys.stdout.write("Graph: " + str(graph.id) + " is iso with: ")
+    #     sys.stdout.flush()
+    #     for gr in main_graphs[graph]:
+    #         sys.stdout.write(str(gr.id) + ", ")
+    #     sys.stdout.write("\n")
+    #     sys.stdout.flush()
     return main_graphs
 
 
@@ -305,6 +427,7 @@ def colorize(main_list: "list", index: int):
                             l = list()
                             v.color_next = index
                             index += 1
+                            # print("newcolor: {}".format(index))
                             l.append(v)
                             copy_to_split.append(l)
                             changed = True
@@ -333,8 +456,6 @@ def colorize(main_list: "list", index: int):
 
 def is_bijection(main_list: list, graph1: Graph, graph2: Graph):
     for listVert in main_list:
-        if len(listVert) < 2:
-            continue
         found_graphs = list()
         for vertice in listVert:
             if vertice.graph != graph1 and vertice.graph != graph2:
@@ -409,18 +530,31 @@ def is_done(lists: "list"):
     return True
 
 
-with open('input/modulesC.grl') as _file:
+# with open('input/products72.grl') as _file:
+#     g, o = read_graph_list(Graph, _file)
+# with open('output/cubes.dot', 'w') as _file:
+#     write_dot(g[0], _file)
+# timeStart = time.time()
+# colorize_list(g, False)
+# # print(time.time() - timeStart)
+with open(input_file) as _file:
     g, o = read_graph_list(Graph, _file)
-print(g)
+
 i = 0
 for graph in g:
     graph.id = i
     i += 1
-timeStart = time.time()
-colorize_list(g)
-print(time.time() - timeStart)
-
-
-
-
-
+if mode == 0:
+    colorize_list(g, True)
+elif mode == 1:
+    with open(input_file) as _file:
+        g2, o = read_graph_list(Graph, _file)
+    for graph in g2:
+        g.append(graph)
+    i = 0
+    for graph in g:
+        graph.id = i
+        i += 1
+    colorize_list(g, True, True)
+elif mode == 2:
+    colorize_list(g, False)
